fd946c271ce7c7a65acd5454e7a002afdec806bc
MimiMicek <makaveli_girl1996@hotmail.com>
2019-05-13 17:01:07 +0200

    Login/Register styling

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 ini_set('display_errors', 0);
 
+//TODO session destory when logging out
+
+//session_destroy(); when click on logout
+
 session_start();
 
 $sUserId = $_SESSION['sUserId'];
--- top.php
+++ top.php
@@ -12,6 +12,7 @@
     <title>Bank</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" type="text/css" href="css/register.css">
+    <link href="https://fonts.googleapis.com/css?family=Jura:500" rel="stylesheet">
 <body>
 
